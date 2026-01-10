35de3b673b4af556a38dde9df29b55605c3c7f98
Krishan Kumar Yadav <krishkydv77@gmail.com>
2026-01-11 01:34:27 +0530

    Update q6_zigzag_pattern.py
    hello friend , please check my method  and analysis and commit.
    my code short ha fast then your code

--- while-loop-practice/q6_zigzag_pattern.py
+++ while-loop-practice/q6_zigzag_pattern.py
@@ -15,3 +15,15 @@ for i in range(1,n-5):
             else:
                 print(" ",end = ' ')
         print('')
+
+
+My Method same Question:-
+
+n= int(input("Enter Number : "))
+for row in range(1,n-2):
+    for col in range(1,n):
+        if(row+col)%2==0:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
